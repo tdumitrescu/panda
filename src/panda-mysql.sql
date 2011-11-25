5fc50affb7d3f80df5f50c694b271338d90de3bd
Ted <miscmisc@cmme.org>
2011-11-24 19:41:24 -0800

    DB structure file

---
+++ src/panda-mysql.sql
@@ -0,0 +1,33 @@
+/*
+SQLyog Community v9.33 GA
+MySQL - 5.5.18 : Database - panda
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`panda` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `panda`;
+
+/*Table structure for table `public_mp3s` */
+
+DROP TABLE IF EXISTS `public_mp3s`;
+
+CREATE TABLE `public_mp3s` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `public_name` varchar(255) NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
